dc3fc269c3c321009790acfe10621d40087f1978
VladyslavLata <vladlatahtc@gmail.com>
2023-03-15 17:50:52 +0200

    add styles on Courses page

--- src/components/CoursePreview/CoursePreview.js
+++ src/components/CoursePreview/CoursePreview.js
@@ -1,39 +1,64 @@
 import ReactPlayer from 'react-player';
+import styles from './CoursePreview.module.scss';
 
 export const CoursePreview = ({ course }) => {
   const {
-    id,
+    // id,
     title,
-    description,
+    // description,
     lessonsCount,
     rating,
     meta,
     previewImageLink,
   } = course;
+
+  const ratingStyles = rating >= 4 ? 'rating-l' : 'rating-m';
   return (
     <>
-      <h2>{title}</h2>
-      <p> {meta.courseVideoPreview?.link}</p>
+      {/* <h2 className={styles.title}>Course</h2> */}
+      <h2 className={styles.title}>{title}</h2>
       {/* <img src={`${courseVideoPreview.link}`} /> */}
-      {/* <video
-        width="250px"
-        height="250px"
-        controls
-        // poster={`${previewImageLink}/cover.webp`}
-        // src={
-        //   'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-        // }
-        // src={meta.courseVideoPreview?.link}
-      >
-        <source src={meta.courseVideoPreview?.link} type="video/mp4" />
+      <div className={styles['course-content-wrapp']}>
+        <div className={styles['video-player-wrapp']}>
+          <video
+            // controls
+            muted
+            width="100%"
+            poster={`${previewImageLink}/cover.webp`}
+            src={
+              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+            }
+            // src={meta.courseVideoPreview?.link}
+          >
+            {/* <source src={meta.courseVideoPreview?.link} type="video/mp4" />
         <source src={meta.courseVideoPreview?.link} type="video/m3u" />
-        <source src={meta.courseVideoPreview?.link} type="video/ogg" />
-      </video> */}
-      <ReactPlayer
+        <source src={meta.courseVideoPreview?.link} type="video/ogg" /> */}
+          </video>
+        </div>
+
+        <div className={styles['footer-course']}>
+          <div className={styles['leson-rating-wrapp']}>
+            <p>
+              <span className={styles['characteristics-accent']}>Lesons:</span>
+              {` ${lessonsCount}`}
+            </p>
+            <p className={`${styles.rating} ${styles[ratingStyles]}`}>
+              {rating}
+            </p>
+          </div>
+          <p className={styles['characteristics-accent']}>Skills:</p>
+          <ul className={styles['skills-list']}>
+            {meta?.skills?.map((skil, i) => (
+              <p key={`${skil}${i}`}>{skil}</p>
+            ))}
+          </ul>
+        </div>
+        {/* <ReactPlayer
         url={meta.courseVideoPreview?.link}
         width={200}
         height={200}
-      />
+      /> */}
+      </div>
     </>
   );
 };
--- src/components/CoursesList/CoursesList.js
+++ src/components/CoursesList/CoursesList.js
@@ -1,16 +1,20 @@
 import { Link } from 'react-router-dom';
 import { CoursePreview } from 'components/CoursePreview/CoursePreview';
+import styles from './CoursesList.module.scss';
 
 export const CoursesList = ({ courses }) => {
   return (
-    <ul>
+    <ul className={styles['courses-list']}>
       {courses.map(course => {
         const { id, meta } = course;
         return (
-          <li key={id}>
-            {/* <Link to={`${id}?course=${meta.slug.toLowerCase()}`}> */}
-            <CoursePreview course={course} />
-            {/* </Link> */}
+          <li key={id} className={styles['course-item']}>
+            <Link
+              to={`${id}?course=${meta.slug.toLowerCase()}`}
+              className={styles.link}
+            >
+              <CoursePreview course={course} />
+            </Link>
           </li>
         );
       })}
